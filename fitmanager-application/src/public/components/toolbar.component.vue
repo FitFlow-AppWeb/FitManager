cfe15b222b09622191b7386ef08c7a684a56c4da
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-08 15:00:06 -0500

    feat(toolbar): improve search input styling and update profile button layout

--- fitmanager-application/src/public/components/toolbar.component.vue
+++ fitmanager-application/src/public/components/toolbar.component.vue
@@ -12,17 +12,13 @@
   <Card>
     <Toolbar class="app-toolbar">
       <template #start>
-        <IconField>
+        <IconField class="toolbar-search">
           <InputIcon class="pi pi-search" />
-          <InputText placeholder="Búsqueda" />
+          <InputText class="search-input-text" placeholder="Search" />
         </IconField>
       </template>
-      <template #center>
-        <Button label="Test"/>
-        <pv-button label="Pv-Button-Test"/>
-      </template>
       <template #end>
-        <span class="temp-text">Profile</span>
+        <Button class="toolbar-profile-pic" variant="outlined" icon="pi pi-user"/>
       </template>
     </Toolbar>
   </Card>
@@ -32,8 +28,7 @@
 
 Card {
   display: flex;
-  background-color: gray;
-  height: 68px;
+  height: 70px;
   width: 100%;
 }
 
@@ -42,10 +37,35 @@ Card {
   justify-content: space-between;
   align-items: center;
   width: 100%;
+  border-bottom-left-radius: 0px;
+  border-top-left-radius: 0px;
+  padding: 0 50px;
 }
 
-.temp-text {
-  color:black;
+:deep(.search-input-text:focus) {
+  background-color: #e6e6e6	;
+}
+
+:deep(.p-button.p-button-outlined), :deep(.p-button.p-button-outlined .p-button-icon) {
+  border-color: #96bcbd;
+    color: #96bcbd;
+}
+
+:deep(.p-button.p-button-outlined:hover) {
+  background-color: #759293;
+  border-color: #759293;
+}
+:deep(.p-button.p-button-outlined:hover .p-button-icon) {
+  color: #ffffff;
+}
+
+:deep(.p-inputtext:focus), :deep(.p-inputwrapper:focus-within) {
+  border-color: #94A3B8 !important;
+}
+
+:deep(.p-button:focus), :deep(.p-element:focus) {
+  outline: 1px solid #96bcbd !important;
+  box-shadow: 0 0 0 0.2rem #a7d1d2 !important;
 }
 
 </style>
