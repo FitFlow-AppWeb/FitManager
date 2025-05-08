dd794382f4524ea14fdc4333a3e2d5447e65dff9
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-08 04:45:45 -0500

    feat(main): integrate router and PrimeVue for enhanced functionality

--- fitmanager-application/src/main.js
+++ fitmanager-application/src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router/index.js'
+import PrimeVue from 'primevue/config'
+import 'primeicons/primeicons.css';
 
-createApp(App).mount('#app')
+// TODO: Add PrimeVue theme
+
+
+
+createApp(App)
+    .use(router)
+    .use(PrimeVue)
+    .mount('#app')
