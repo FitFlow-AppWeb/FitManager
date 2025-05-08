f71c40fc31df9bbe30cb948f6e281e15b854be04
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-08 11:22:01 -0500

    feat(theme): integrate Aura theme with PrimeVue for enhanced styling options

--- fitmanager-application/src/main.js
+++ fitmanager-application/src/main.js
@@ -4,12 +4,18 @@ import App from './App.vue'
 import router from './router/index.js'
 import PrimeVue from 'primevue/config'
 import 'primeicons/primeicons.css';
-
+import Aura from '@primevue/themes/aura'
 // TODO: Add PrimeVue theme
 
-
-
 createApp(App)
+    .use(PrimeVue, {
+        ripple: true,
+        theme: {
+            preset: Aura,
+            options: {
+                darkModeSelector: false
+            }
+        }
+    })
     .use(router)
-    .use(PrimeVue)
     .mount('#app')
