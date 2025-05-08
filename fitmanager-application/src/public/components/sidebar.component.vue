53b248ab3cf5856e425a21a53da3bbb6c8710a33
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-08 04:44:08 -0500

    feat(sidebar): add sidebar component with navigation menu

---
+++ fitmanager-application/src/public/components/sidebar.component.vue
@@ -0,0 +1,176 @@
+<script setup>
+import {Menu} from "primevue";
+import {ref} from "vue";
+
+import DashboardIcon from '/assets/Chart_fill.svg?raw';
+import CalendarIcon from '/assets/Date_range_fill.svg?raw';
+import PersonalIcon from '/assets/File_dock_fill.svg?raw';
+import MembersIcon from '/assets/Group_fill.svg?raw';
+import ClassesIcon from '/assets/Desk_alt_fill.svg?raw';
+import BookingsIcon from '/assets/notebook_fill.svg?raw';
+import FinancesIcon from '/assets/Credit_card_fill.svg?raw';
+import AttendanceIcon from '/assets/Time_fill.svg?raw';
+import NotificationsIcon from '/assets/Bell_fill.svg?raw';
+
+
+
+
+const menuItems = ref([
+  {
+    label: 'Dashboard',
+    svgContent: DashboardIcon,
+    to: '/dashboard'
+  },
+  {
+    label: 'Calendar',
+    svgContent: CalendarIcon,
+    to: '/calendar'
+  },
+  {
+    label: 'Personal',
+    svgContent: PersonalIcon,
+    to: '/employees'
+  },
+  {
+    label: 'Members',
+    svgContent: MembersIcon,
+    to: '/members'
+  },
+  {
+    label: 'Classes',
+    svgContent: ClassesIcon,
+    to: '/classes'
+  },
+  {
+    label: 'Bookings',
+    svgContent: BookingsIcon,
+    to: '/bookings'
+  },
+  {
+    label: 'Finances',
+    svgContent: FinancesIcon,
+    to: '/finances'
+  },
+  {
+    label: 'Attendance',
+    svgContent: AttendanceIcon,
+    to: '/attendance'
+  },
+  {
+    label: 'Notifications',
+    svgContent: NotificationsIcon,
+    to: '/notifications'
+  }
+]);
+
+
+</script>
+
+<template>
+  <div class="app-sidebar">
+    <div class="sidebar-header">
+      <h3 class="app-title">FITMANAGER</h3>
+      <img src="/assets/logo-app.png" alt="FitManager Logo" class="app-logo">
+    </div>
+    <div class="line-separator"></div>
+    <div class="sidebar-nav">
+      <Menu class="nav-contentMenu" :model="menuItems">
+        <template #itemicon="{ item }">
+          <div v-if="item.svgContent"
+               class="custom-svg-container"
+               v-html="item.svgContent"> </div>
+          <span v-else-if="item.icon" :class="['p-menuitem-icon', item.icon]"></span>
+        </template>
+      </Menu>
+    </div>
+    <div class="line-separator"></div>
+    <div class="sidebar-footer">
+      <!--      TODO :  LOGOUT -->
+      <p>Salir</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app-sidebar {
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  height: 100vh;
+  background-color: #A7D1D2;
+  color: #505F5F;
+  box-shadow: 2px 0 5px rgba(0,0,0,0.1);
+}
+
+.sidebar-header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0;
+  .app-title {
+    font-size: 2rem;
+    font-family: 'Bebas Neue',serif;
+    font-weight: bold;
+    margin: 0;
+    padding-right: 10px;
+  }
+  .app-logo {
+    height: 36px;
+    width: 51px;
+  }
+}
+
+.line-separator {
+  width: calc(100% - 20px);
+  margin: 0 auto;
+  height: 1px;
+  background-color: rgba(58, 73, 73, 0.5);
+}
+
+.sidebar-nav {
+  flex-grow: 1;
+  padding: 0 10px;
+  .nav-contentMenu {
+    width: 100%;
+  }
+}
+
+.nav-contentMenu {
+  width: 100%;
+  background-color: transparent !important;
+  padding: 0 !important;
+}
+
+:deep(.p-menu-item-link) {
+  margin: 15px 20px;
+}
+
+:deep(.p-menu-item-label) {
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+  font-weight: 400;
+}
+
+.nav-contentMenu :deep(.custom-svg-container) {
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+}
+
+.nav-contentMenu :deep(.custom-svg-container svg) {
+  width: 3rem !important;
+  height: 3rem !important;
+}
+
+.nav-contentMenu :deep(svg) {
+  width: 3rem !important;
+  height: 3rem !important;
+}
+
+.sidebar-footer {
+  margin: 0 15px;
+  text-align: center;
+}
+
+</style>
