3b860ad588d8da2d00b7c46f3e03f36d0343add7
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-08 11:22:17 -0500

    feat(toolbar): enhance toolbar layout with search input and profile section

--- fitmanager-application/src/public/components/toolbar.component.vue
+++ fitmanager-application/src/public/components/toolbar.component.vue
@@ -1,21 +1,51 @@
 <script setup>
+  import {ref} from "vue";
+  import Toolbar from 'primevue/toolbar';
+  import IconField from 'primevue/iconfield';
+  import InputIcon from 'primevue/inputicon';
+  import InputText from "primevue/inputtext";
+  import Button from 'primevue/button';
 
 </script>
 
 <template>
-  <div class="app-toolbar">
-    <span>Busqueda</span>
-    <Span>Perfil</Span>
-  </div>
+  <Card>
+    <Toolbar class="app-toolbar">
+      <template #start>
+        <IconField>
+          <InputIcon class="pi pi-search" />
+          <InputText placeholder="Búsqueda" />
+        </IconField>
+      </template>
+      <template #center>
+        <Button label="Test"/>
+        <pv-button label="Pv-Button-Test"/>
+      </template>
+      <template #end>
+        <span class="temp-text">Profile</span>
+      </template>
+    </Toolbar>
+  </Card>
 </template>
 
 <style scoped>
+
+Card {
+  display: flex;
+  background-color: gray;
+  height: 68px;
+  width: 100%;
+}
+
 .app-toolbar {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
-  height: 68px;
-  background-color: #FFFFFF;
+  width: 100%;
+}
+
+.temp-text {
   color:black;
 }
+
 </style>
