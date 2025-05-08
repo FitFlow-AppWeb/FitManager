95af0151e4ab56dcd859402c2bd8e40cef75c5ff
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-08 04:44:25 -0500

    feat(toolbar): add toolbar component with search and profile options (still TODO)

---
+++ fitmanager-application/src/public/components/toolbar.component.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="app-toolbar">
+    <span>Busqueda</span>
+    <Span>Perfil</Span>
+  </div>
+</template>
+
+<style scoped>
+.app-toolbar {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 68px;
+  background-color: #FFFFFF;
+  color:black;
+}
+</style>
