5e3b98d2a397565714f69c55d2e0577f94b48f38
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-09 15:49:20 -0500

    feat(graphic): add card layout and styling for improved data presentation

--- fitmanager-application/src/finances/components/graphic.component.vue
+++ fitmanager-application/src/finances/components/graphic.component.vue
@@ -1,13 +1,16 @@
 <script>
-export default {
-  name: "graphic.component"
-}
+
 </script>
 
 <template>
-
+  <div class="card">
+    <Chart type="line" :data="chartData" :options="chartOptions" class="h-[30rem]" />
+    <span>GraphData</span>
+  </div>
 </template>
 
 <style scoped>
-
+.card{
+  background-color: #94A3B8;
+}
 </style>
