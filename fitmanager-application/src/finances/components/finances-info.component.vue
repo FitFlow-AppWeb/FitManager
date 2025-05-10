114f79778afdaf39a1f4c3e2df588d20a927c8bf
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-05-10 00:33:41 -0500

    feat(finances): enhance FinancesInfo component with card layout and divider

--- fitmanager-application/src/finances/components/finances-info.component.vue
+++ fitmanager-application/src/finances/components/finances-info.component.vue
@@ -1,11 +1,27 @@
 <script>
+import {defineComponent} from "vue";
+import {Divider as PvDivider} from "primevue";
+
+export default defineComponent({
+  components: {PvDivider}
+})
 
 </script>
 
 <template>
-  <div class="card">
-    <p>FinancesInfo</p>
-  </div>
+  <pv-card>
+    <template #title>
+      General Information
+      <pv-divider/>
+    </template>
+    <template #content>
+      Finances Info
+    </template>
+
+<!--    <template #footer>-->
+<!--&lt;!&ndash;      <pv-button @click="$emit('close')">Download Info</pv-button>&ndash;&gt;-->
+<!--    </template>-->
+  </pv-card>
 </template>
 
 <style scoped>
