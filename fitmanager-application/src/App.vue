5fd6b436a86aa7e008908c75f98444c96e58e012
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-07-06 10:43:06 -0500

    feat(auth): add AuthUser entity and authentication service for sign-in and sign-up

--- fitmanager-application/src/App.vue
+++ fitmanager-application/src/App.vue
@@ -1,28 +1,30 @@
 <script setup>
-import { ref } from 'vue';
-import Login from './login/components/login.component.vue'; // Componente de Login
-import Layout from './public/components/app-layout.componet.vue'; // Componente Layout
-
-// Inicializa el estado como false para que el Login se muestre por defecto
-const isAuthenticated = ref(false); // No usamos localStorage aquí, solo el estado interno
-
-// Método para manejar el evento de login exitoso
-const handleLoginSuccess = (authenticated) => {
-  if (authenticated) {
-    isAuthenticated.value = true; // Cambiar el estado a autenticado
-    console.log("Login exitoso: Usuario autenticado.");
-  }
-};
+// import { ref } from 'vue';
+// import Login from './login/components/login.component.vue'; // Componente de Login
+// import Layout from './public/components/app-layout.componet.vue'; // Componente Layout
+//
+// // Inicializa el estado como false para que el Login se muestre por defecto
+// const isAuthenticated = ref(false); // No usamos localStorage aquí, solo el estado interno
+//
+// // Método para manejar el evento de login exitoso
+// const handleLoginSuccess = (authenticated) => {
+//   if (authenticated) {
+//     isAuthenticated.value = true; // Cambiar el estado a autenticado
+//     console.log("Login exitoso: Usuario autenticado.");
+//   }
+// };
 </script>
 
 <template>
-  <div id="app">
-    <!-- Mostrar Login si el usuario no está autenticado -->
-    <Login v-if="!isAuthenticated" @login-exitoso="handleLoginSuccess" />
+  <router-view/>
 
-    <!-- Mostrar Layout si el usuario está autenticado -->
-    <Layout v-if="isAuthenticated" />
-  </div>
+  <!--  <div id="app">-->
+<!--    &lt;!&ndash; Mostrar Login si el usuario no está autenticado &ndash;&gt;-->
+<!--    <Login v-if="!isAuthenticated" @login-exitoso="handleLoginSuccess" />-->
+
+<!--    &lt;!&ndash; Mostrar Layout si el usuario está autenticado &ndash;&gt;-->
+<!--    <Layout v-if="isAuthenticated" />-->
+<!--  </div>-->
 </template>
 
 <style scoped>
